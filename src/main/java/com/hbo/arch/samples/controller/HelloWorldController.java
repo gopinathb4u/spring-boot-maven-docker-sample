e2a149537e4388a9e7aa526e6ff0a9f8e782a9d2
ggovinda <gopinath.govindaraman@hbo.com>
2020-04-09 13:19:51 +0530

    Package name changed

--- src/main/java/com/hbo/arch/samples/controller/HelloWorldController.java
+++ src/main/java/com/hbo/arch/samples/controller/HelloWorldController.java
@@ -1,13 +1,12 @@
-package com.hbo.codecommit.controller;
+package com.hbo.arch.samples.controller;
 
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
-    @RequestMapping(value = "/hello" , method = RequestMethod.GET)
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello() {
         return "hello";
     }
